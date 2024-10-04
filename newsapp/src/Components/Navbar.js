f5fed8f3d5fa2e231a53eb622475c05bdb2f9440
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-10-04 23:24:42 +0530

    Initial Commit

--- newsapp/src/Components/Navbar.js
+++ newsapp/src/Components/Navbar.js
@@ -5,6 +5,7 @@ import { CategoryState } from '../context/GlobalContext';
 export default function Navbar() {
     const { setcategory, setsearch } = CategoryState();
     const [searchInput, setsearchInput] = useState('')
+    const [hide, sethide] = useState(false)
 
     const handleinput = (e)=>{
         setsearchInput(e.target.value)
@@ -15,50 +16,51 @@ export default function Navbar() {
         setsearch(searchInput)
         setsearchInput('');
     }
+
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100" style={{ position: 'fixed', top: '0', zIndex: "1" }}>
                 <div className="container-fluid">
                     <h1 className="navbar-brand mt-1 fst-italic fw-bold fs-1">NewTidings</h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon" onClick={()=>sethide(false)}></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse" id="navbarNav" style={{display:(hide?'none':"")}}>
                         <ul className="navbar-nav">
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/NewTidings" onClick={() => setcategory('top')}>Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="/NewTidings" onClick={()=> {setcategory('top'); sethide(true)}}>Home</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/buisness" onClick={() => setcategory('business')}>Buisness</Link>
+                                <Link className="nav-link active" aria-current="page" to="/buisness" onClick={() => {setcategory('business'); sethide(true)}}>Buisness</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/crime" onClick={() => setcategory('crime')}>Crime</Link>
+                                <Link className="nav-link active" aria-current="page" to="/crime" onClick={() => {setcategory('crime'); sethide(true)}}>Crime</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/education" onClick={() => setcategory('education')}>Education</Link>
+                                <Link className="nav-link active" aria-current="page" to="/education" onClick={() => {setcategory('education'); sethide(true)}}>Education</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/entertainment" onClick={() => setcategory('entertainment')}>Entertainment</Link>
+                                <Link className="nav-link active" aria-current="page" to="/entertainment" onClick={() => {setcategory('entertainment'); sethide(true)}}>Entertainment</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/health" onClick={() => setcategory('health')}>Health</Link>
+                                <Link className="nav-link active" aria-current="page" to="/health" onClick={() => {setcategory('health'); sethide(true)}}>Health</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/politics" onClick={() => setcategory('politics')}>Politics</Link>
+                                <Link className="nav-link active" aria-current="page" to="/politics" onClick={() => {setcategory('politics'); sethide(true)}}>Politics</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/sports" onClick={() => setcategory('sports')}>Sports</Link>
+                                <Link className="nav-link active" aria-current="page" to="/sports" onClick={() => {setcategory('sports'); sethide(true)}}>Sports</Link>
                             </li>
 
                             <li className="nav-item fs-5 fw-bolder">
-                                <Link className="nav-link active" aria-current="page" to="/technology" onClick={() => setcategory('technology')}>Technology</Link>
+                                <Link className="nav-link active" aria-current="page" to="/technology" onClick={() => {setcategory('technology'); sethide(true)}}>Technology</Link>
                             </li>
                         </ul>
 
